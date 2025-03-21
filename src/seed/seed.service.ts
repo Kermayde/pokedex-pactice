477c27cf1e590b674c26c897cb455dd55149efce
Carlos Flores <carlosdelgado_09@hotmail.com>
2025-03-20 23:20:03 -0600

    feat: ✨insert pokemon by lote

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,22 +1,35 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class SeedService {
+
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>
+  ) {}
+  
 
   private readonly axios: AxiosInstance = axios;
 
 
   async runSeed() {
     const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10');
-    data.results.forEach(({ name, url }) => {
+    data.results.forEach(async({ name, url }) => {
       const segments = url.split('/');
       const no = +segments[segments.length - 2];
 
-      console.log(`No: ${no}, Name: ${name}`);
+      name = name.toUpperCase();
+      try {
+        await this.pokemonModel.create({ no, name });
+      } catch (error) {
+        console.log(error);
+      }
     })
-    
-    return data.results;
+    return 'Seed executed';
   }
 }
