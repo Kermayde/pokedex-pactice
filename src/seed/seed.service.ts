56f708c912ca5515ea94f8fee3513a843895e9ad
Carlos Flores <carlosdelgado_09@hotmail.com>
2025-03-20 22:29:51 -0600

    feat: ✨ make http request

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
-  runSeed() {
-    return 'Seed has been run';
+
+  private readonly axios: AxiosInstance = axios;
+
+
+  async runSeed() {
+    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10');
+    data.results.forEach(({ name, url }) => {
+      const segments = url.split('/');
+      const no = +segments[segments.length - 2];
+
+      console.log(`No: ${no}, Name: ${name}`);
+    })
+    
+    return data.results;
   }
 }
